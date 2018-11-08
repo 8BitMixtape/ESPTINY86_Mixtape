300128f177120630d64cc5f744bfeb71a0cb798b
ChrisMicro <christoph@roboterclub-freiburg.de>
2018-11-08 18:20:14 +0100

    PDM no dac RC low pass driver moved to I2S driver

--- code/BeriBeriCool_noDAC-NEO_serial/BeriBeriCool_noDAC-NEO_serial.ino
+++ code/BeriBeriCool_noDAC-NEO_serial/BeriBeriCool_noDAC-NEO_serial.ino
@@ -17,115 +17,78 @@
   (at your option) any later version.
   ********************* list of outhors **********************************
 
-  v0.1   24.August.2018  ChrisMicro  initial version
+  v0.1   24.August.2018   ChrisMicro  initial version
+  v0.2   xxOctober 2018   badgeek, multiplexer moved to library
+  v0.3   5.November.2018  ChrisMicro, I2S format addapted to PT8211 I2S DAC
+  v0.4   6.November 2018  dusjagr adding NEO-pixel, PDM and serial
+  v0.5   8.November 2018  ChrisMicro, PDM moved to I2S driver
 
   It is mandatory to keep the list of authors in this code.
   Please add your name if you improve/extend something
 
 */
 
-#include <Arduino.h>
-#include <i2s.h>
-#include <i2s_reg.h>
+#include "hardwarePlatform.h"
 #include "AnalogMultiplexer.h"
 #include "synthx.h" // change this for other synth patch
-#include "ESP8266WiFi.h" // wifi header only needed to turn it off
-#include <Adafruit_NeoPixel.h> 
+#include "I2S.h"
 
-#define PIN D7
 
-#define NUM_LEDS 8
+#include <Adafruit_NeoPixel.h>
+
+I2SClass I2S;
 
 #define BRIGHTNESS 30
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEOPIXELNUMLEDS, NEOPIXELPIN, NEO_GRB + NEO_KHZ800);
 
 byte neopix_gamma[] = {
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
-    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
-    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
-   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
-   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
-   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
-   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
-   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
-   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
-  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
-  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
-  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
-  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
-
-
-//#include <OSCMessage.h>
-//#include <OSCBundle.h>
-//#include <OSCData.h>
-
-extern "C" {
-#include "user_interface.h"
-}
-
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+  2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+  5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+  10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+  17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+  25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+  37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+  51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+  69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+  90, 92, 93, 95, 96, 98, 99, 101, 102, 104, 105, 107, 109, 110, 112, 114,
+  115, 117, 119, 120, 122, 124, 126, 127, 129, 131, 133, 135, 137, 138, 140, 142,
+  144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 167, 169, 171, 173, 175,
+  177, 180, 182, 184, 186, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213,
+  215, 218, 220, 223, 225, 228, 231, 233, 236, 239, 241, 244, 247, 249, 252, 255
+};
 
 #define SAMPLINGFREQUENCY 44100
-
-// multipexer select pins
-#define MULTIPLEXED_ANALOG_INPUT A0
-#define MUX_A D0
-#define MUX_B D1
-#define MUX_C D2
-
-//PDM From Jan Ostman
-uint32_t i2sACC;
-uint16_t err;
 
 AnalogMultiplexerPin multiplexer;
 
 SynthTest mysynth;
-
-void writeDAC(uint16_t DAC) {
-  for (uint8_t i=0;i<32;i++) {
-    i2sACC=i2sACC<<1;
-    if(DAC >= err) {
-      i2sACC|=1;
-      err += 0xFFFF-DAC;
-    } else {
-      err -= DAC;
-    }
-  }
-  bool flag=i2s_write_sample(i2sACC);
-}
-
 
 void setup()
 {
   WiFi.forceSleepBegin(); // turn of wifi to reduce power consumption
   delay(1);
   system_update_cpu_freq(160); // run MCU core with full speed
-  Serial.begin(230400);
-  pinMode(2, INPUT); //restore GPIOs taken by i2s
-  pinMode(15, INPUT);
-  
-  i2s_begin();
-  i2s_set_rate(SAMPLINGFREQUENCY);
+  Serial.begin(115200);
 
-  // synthesizer input parameters
-  // usually connected to potis
-  // range: 0..1024
-  // this parameters can be set on the fly in the slow loop
-
-  multiplexer.setup(MUX_A, MUX_B, MUX_C, MULTIPLEXED_ANALOG_INPUT);
+  // PT8211 needs I2S_LEFT_JUSTIFIED_MODE
+  //I2S.begin(I2S_LEFT_JUSTIFIED_MODE, SAMPLINGFREQUENCY, 16);
+  // I2S_PDM_MODE ==> no dac, connect RC-low pass instead
+  I2S.begin(I2S_PDM_MODE, SAMPLINGFREQUENCY, 16);
   
+  multiplexer.setup(MULTIPLEXER_PINA, MULTIPLEXER_PINB, MULTIPLEXER_PINC, ADC);
+
   strip.setBrightness(BRIGHTNESS);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  
+
   pinMode(D6, OUTPUT);
   digitalWrite(D6, LOW); // turn on LED
-  //rainbowCycle (2);
+  rainbowCycle (3);
 }
-
 
 void slowLoop()
 {
@@ -154,9 +117,11 @@ void loop()
   static uint16_t counter = 0;
   uint16_t dacValue;
 
-  dacValue = mysynth.run(cycle++);
-  writeDAC(dacValue^0x8000);
-  //i2s_write_sample(dacValue ^ 0x8000);
+  dacValue = (int32_t)mysynth.run(cycle++) - 0x8000;
+
+
+  // stereo output
+  I2S.write(dacValue,dacValue);
 
   counter++;
 
@@ -165,16 +130,14 @@ void loop()
     counter = 0;
     slowLoop();
   }
-
-
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
 
-  for(j=0; j<256 * 5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
+  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+    for (i = 0; i < strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
     strip.show();
@@ -185,9 +148,9 @@ void rainbowCycle(uint8_t wait) {
 void rainbow(uint8_t wait) {
   uint16_t i, j;
 
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
     }
     strip.show();
     delay(wait);
@@ -196,7 +159,7 @@ void rainbow(uint8_t wait) {
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
     strip.show();
     delay(wait);
@@ -207,15 +170,15 @@ void colorWipe(uint32_t c, uint8_t wait) {
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3,0);
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3, 0);
   }
-  if(WheelPos < 170) {
+  if (WheelPos < 170) {
     WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3,0);
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3, 0);
   }
   WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0,0);
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0, 0);
 }
 
 uint8_t red(uint32_t c) {
---
+++ code/BeriBeriCool_noDAC-NEO_serial/hardwarePlatform.h
@@ -0,0 +1,42 @@
+/*
+  BerryBerryCool platfomr pin definitions
+
+
+  ************************************************************************
+  This sketch is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************* list of outhors **********************************
+
+  v0.1   7. November 2018  ChrisMicro  changed to Adafruit_NeoPixel library
+*/
+
+#include <Arduino.h>
+#include "ESP8266WiFi.h"
+#include "I2S.h"
+#include <Adafruit_NeoPixel.h>
+
+// BeriBeriCool pin definitions
+#define LED               D6 // LED on Mixtape board, low active
+#define NEOPIXELPIN       D7
+#define NEOPIXELNUMLEDS   8
+#define MULTIPLEXER_PINA  D0
+#define MULTIPLEXER_PINB  D1
+#define MULTIPLEXER_PINC  D2
+#define I2S_DATA          RX
+#define I2S_BCLK          D8
+#define I2S_WS            D4
+#define ADC               A0
+#define BUTTONS_ADC       ADC
+
+//MULTIPLEXER_CHANNEL
+#define POTI1   0
+#define POTI2   1
+#define POTI3   2
+#define POTI4   3
+#define CV1     4
+#define CV2     5
+#define BUTTONS 6
+
+
